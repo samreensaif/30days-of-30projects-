6aa7f26e2f755e02bb1eae7248fbcfea710b5664
samreensaif <160466969+samreensaif@users.noreply.github.com>
2024-09-16 20:02:33 +0500

    update tailwind.config.ts

--- day01-countdown-timer-app/tailwind.config.ts
+++ day01-countdown-timer-app/tailwind.config.ts
@@ -55,7 +55,15 @@ const config: Config = {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
-  		}
+  		},
+		  screens: {
+			'sm': '640px',
+			'md': '768px',
+			'lg': '1024px',
+			'xl': '1280px',
+			'2xl': '1536px',
+			'3xl': '1920px',
+		  },
   	}
   },
   plugins: [require("tailwindcss-animate")],
